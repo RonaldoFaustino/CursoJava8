f99d96b960d8a04b9b33b341c1c1598e7617049e
Ronaldo Faustino <ohnirucse@msn.com>
2018-06-06 22:29:48 -0300

    POO2

---
+++ 1Disciplina/src/unidade3/UnidadeDeExercicio.java
@@ -0,0 +1,20 @@
+package unidade3;
+
+public class UnidadeDeExercicio {
+
+	public static void main(String[] args) {
+		String nome = "Curso de Java";
+		String avaliacao = " é otimo";
+		String data = "24/02/12";
+		
+		System.out.println(nome + avaliacao);
+
+		String nome1= nome.substring(9,13);
+		System.out.println(nome1);
+		
+				
+}
+        
+	}
+
+
