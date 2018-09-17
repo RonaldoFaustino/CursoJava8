27c79d382d08fd1cbce9fe27d03bf356444b2355
Ronaldo Faustino <ohnirucse@msn.com>
2018-09-16 23:30:30 -0300

    Exceptions

---
+++ 2Disciplina/src/unidade1/Calculadora.java
@@ -0,0 +1,28 @@
+package unidade1;
+
+import java.util.Scanner;
+
+public class Calculadora {
+	
+	public static void dividir(int n1, int n2)throws ArithmeticException {
+		
+		System.out.println(n1/n2);
+	}
+	
+	public static void mains(String[]args){
+		
+		
+		Scanner entrada = new Scanner (System.in);
+		System.out.println("Digite N1");
+				int n1 = entrada.nextInt();
+		System.out.println("Digite n2");
+				int n2 = entrada.nextInt();
+				try{
+					dividir(n1,n2);
+				}
+				catch(ArithmeticException e)
+				{
+					System.out.println("Erro divisão por zero!!!");
+				}
+	}
+}
--- 2Disciplina/src/unidade1/Test.java
+++ 2Disciplina/src/unidade1/Test.java
@@ -2,19 +2,22 @@ package unidade1;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws ArrayIndexOutOfBoundsException{
 
 		int i = 0;
 		String frases []= {"um","dois","tres"}; //,"quatro","cinco","seis"}; 
 		while (i<4)
 		{
-			try{
+			/*try{
 				System.out.println(frases[i]);
 			}
 			catch (ArrayIndexOutOfBoundsException e)
 			{
 				System.out.println("Erro na acesso ao vetor");
 			}
+			i++;*/
+			System.out.println(frases[i]);
+			
 			i++;
 		}
 	}
